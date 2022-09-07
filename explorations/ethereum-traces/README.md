a3a2e50cf07f867fa6b0a3bdc65afa482d385008
tjayrush <jrush@quickblocks.io>
2022-09-07 11:25:41 -0400

    Cleans up ethereum-traces a bit

--- explorations/ethereum-traces/README.md
+++ explorations/ethereum-traces/README.md
@@ -1,2 +1,3 @@
 # Trueblocks Analytics
+
 This project explores the Ethereum chain summary data Jay generated in the folder: Great-Hill-Corporation/trueblocks-tokenomics/other_data
--- explorations/ethereum-traces/explore.R
+++ explorations/ethereum-traces/explore.R
@@ -1,32 +1,51 @@
-# This file is in the public domain
 require(tidyverse)
-require(viridisLite)
+require(ggplot2)
 
-data.dir <- "../../trueblocks-tokenomics/other_data/"
+data_dir <- "explorations/ethereum-traces/store/"
 
-countsByWeek <- read_tsv(paste0(data.dir, "countsByWeek.txt"))
-
-  
-
-
-Traces <- readxl::read_xlsx(paste0(data.dir, "Traces.xlsx"), range = "A5:CW255") %>%
+traces <- readxl::read_xlsx(paste0(data_dir, "traces.xlsx"), range = "A5:CW255") %>%
   rename(nTraces = `...1`) %>%
   gather(block.bucket, occurrences, -nTraces) %>%
   mutate(block.bucket = as.integer(block.bucket))
 
-Traces %>%
-  #mutate(block.bucket = ntile(block.bucket, 25) * 2000000) %>%
+traces %>%
+  # mutate(block.bucket = ntile(block.bucket, 25) * 2000000) %>%
   ggplot(aes(x = block.bucket, y = nTraces, fill = occurrences)) +
-  geom_tile(color = "white", size=0) + 
-  viridis::scale_fill_viridis(name = "log(n occurrences)", trans="pseudo_log", option = "A", labels=scales::comma) +
-  theme_minimal(base_size=8)
+  geom_tile(color = "white", size = 0) +
+  scale_fill_gradientn(colours = viridis(256, option = "D")) +
+  theme_minimal(base_size = 8)
 
-tokensByWeek <- read_delim(paste0(data.dir, "tokensByWeek.txt"), delim = "|") %>%
+transactions <- readxl::read_xlsx(paste0(data_dir, "transactions.xlsx"), range = "A4:CW254") %>%
+  rename(nTraces = `...1`) %>%
+  gather(block.bucket, occurrences, -nTraces) %>%
+  mutate(nTraces = nTraces * 10) %>%
+  mutate(block.bucket = as.integer(block.bucket))
+
+# names(transactions)
+#  scale_fill_gradientn(name = "log(n occurrences)", trans="pseudo_log", option = "A") +
+transactions %>%
+  mutate(block.bucket = ntile(block.bucket, 25) * 2000000) %>%
+  ggplot(aes(x = block.bucket, y = nTraces, fill = occurrences)) +
+  geom_tile(color = "white", size = 0) +
+  scale_fill_gradientn(colours = viridis(256, option = "D")) +
+  theme_minimal(base_size = 8)
+
+token_by_week <- read_delim(paste0(data_dir, "tokensByWeek.txt"), delim = "|") %>%
   gather("type", "value", c(-blockNum, -date)) %>%
   mutate(value = as.integer(value))
 
-tokensByWeek %>%
+token_by_week %>%
   ggplot(aes(x = blockNum, y = value, color = type)) +
   geom_line() +
-  theme_minimal(base_size=10)
+  scale_fill_gradientn(colours = viridis(256, option = "D")) +
+  theme_minimal(base_size = 10)
 
+count_by_week <- read_tsv(paste0(data_dir, "countsByWeek.txt")) %>%
+  gather("type", "value", c(-blockNum, -date)) %>%
+  mutate(value = as.integer(value))
+
+count_by_week %>%
+  ggplot(aes(x = blockNum, y = value, color = type)) +
+  geom_line() +
+  scale_fill_gradientn(colours = viridis(256, option = "D")) +
+  theme_minimal(base_size = 10)
