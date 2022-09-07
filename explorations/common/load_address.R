6bad5fc3a4b25f0d948036f4efe61d597d601175
tjayrush <jrush@quickblocks.io>
2022-09-06 21:59:19 -0400

    Updating

--- explorations/common/load_address.R
+++ explorations/common/load_address.R
@@ -1,8 +1,21 @@
 require(tidyverse)
 require(scales)
 
-# assumes both the `address` and the `name` values contain valid data
-url <- paste0("http://localhost:8080/list?addrs=", address)
-ret <- url %>% fromJSON()
-ret <- ret$data %>% as_tibble()
-ret <- ret %>% mutate(address = name)
+#' A function
+#'
+#' @param address An Ethereum address
+#' @param name A name to assign to that address
+#'
+#' @return The dataframe containing all the appearances <blockNumber, transactionIndex> for the given address
+#' @importFrom dplyr mutate
+#' @importFrom rlang enquo
+#' @importFrom rlang quo_name
+#' @importFrom rlang :=
+#' @export
+get_address <- function(address, name) {
+    url <- paste0("http://localhost:8080/list?addrs=", address)
+    ret <- url %>% fromJSON()
+    ret <- ret$data %>% as_tibble()
+    ret <- ret %>% mutate(address = name)
+    return ret
+}
